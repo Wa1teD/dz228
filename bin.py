fc982a87b8cb6ce83252e5eae32e19835c135f99
Wa1teD <90134186+Wa1teD@users.noreply.github.com>
2021-11-29 23:00:18 +0300

    Add files via upload

--- bin.py
+++ bin.py
@@ -6,23 +6,31 @@ def list_n(j):
     while i < j:
         i += 1
         q.append(i)
-    return q 
+    return q
 
 def bin_search(lst, key):
-        if type(lst).__name__ == "list":
+    left = 0 
+    right = len(lst)
+    while left != None:
+        if key > right or key < 0:
             return None
-        else:
-            left = 0 
-            right = len(lst)
-            while left != None:
-                if key > right or key < 0:
-                    return None
-                else: 
-                     middle = trunc((left + right) / 2)
-                     if lst[middle] > key: 
-                         right = middle - 1
-                     elif lst[middle] < key:
-                         left = middle + 1
-                     elif lst[middle] == key:
-                         return "Ваш индекс : " + str(middle)
+        else: 
+            middle = trunc((left + right) / 2)
+            if lst[middle] > key: 
+                right = middle - 1
+            elif lst[middle] < key:
+                left = middle + 1
+            elif lst[middle] == key:
+                return "Ваш индекс : " + str(middle)
+def tests():
+    print(bin_search([1,2,3,4,5],5))#4
+    print(bin_search([69],1))#None
+    print(bin_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],3))#2
+    print(bin_search([0,1,2],0))#0
+    print(bin_search([1],-2))#None
+    print(bin_search([1,2,3,4,3],3))
+tests()
+
+            
+
                         
